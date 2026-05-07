cd2a8d02a03e99176a79d35da7b21b0d202b7562
Colin Djerdak <c.djerdak@gmail.com>
2026-05-07 14:35:14 +0200

    Erweiterung der Komponenten

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,25 @@
 <template>
-  <div className="container">
-    <h1>Home</h1>
-    <p>Willkommen auf der Startseite.</p>
-  
-    <div className="card">
-        <h2>Karte</h2>
-        <p>Dies ist eine normale Karte</p>
+  <section>
+    <div className="container">
+      <h1>Home</h1>
+      <p>Willkommen auf der Startseite.</p>
+
+      <div class="alert alert-success">
+        Das ist eine Erfolgsmeldung. 
+      </div>
+      <div class="alert alert-warning">
+        Das ist eine Warnmeldung. 
+      </div>
+      <div class="alert alert-error">
+        Das ist eine Fehlermeldung. 
+      </div>
+
+      <p><a href="" class="btn btn-primary">Jetzt starten!</a></p>
+    
+      <div className="card">
+          <h2>Karte</h2>
+          <p>Dies ist eine normale Karte</p>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
