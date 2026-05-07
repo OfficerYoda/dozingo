d071c94d8e4b42d0006ec83685f1ded2475e099e
Colin Djerdak <c.djerdak@gmail.com>
2026-05-07 10:13:48 +0200

    Vue frontend init

---
+++ frontend/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <h1><!-- Page Title --></h1>
+    <!-- Sidebar open btn later on -->
+  </header>
+
+  <aside>
+    <div className="sidebar-header">
+      <span>DOZINGO</span>
+      <!-- Sidebar close btn later on -->
+    </div>
+    <div className="sidebar-content">
+      <nav>
+        <ul>
+          <li><RouterLink to="/">Home</RouterLink></li>
+          <li><RouterLink to="/about">Kategorien</RouterLink></li>
+        </ul>
+      </nav>
+    </div>
+  </aside>
+
+  <main>
+    <RouterView />
+  </main>
+
+  <footer>
+    &#169 DOZINGO - Alle Rechte vorbehalten
+  </footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div className="container">
+    <h1>Home</h1>
+    <p>Willkommen auf der Startseite.</p>
+  
+    <div className="card">
+        <h2>Karte</h2>
+        <p>Dies ist eine normale Karte</p>
+    </div>
+  </div>
+</template>
