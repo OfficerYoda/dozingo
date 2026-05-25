a8ff6f2f3760531487ab5ba47d5329af40d0aa4f
Tom Barth <tom.barth@sap.com>
2026-05-25 19:08:39 +0200

    seite gebaut

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,6 +5,7 @@ import SettingsView from '@/views/SettingsView.vue'
 import ProfileView from '@/views/ProfileView.vue'
 import ComponentsView from '@/views/ComponentsView.vue'
 import CardEditorView from '@/views/CardEditorView.vue'
+import CardsView from '@/views/CardsView.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
@@ -12,6 +13,7 @@ const routes = [
   { path: '/profile', name: 'profile', component: ProfileView },
   { path: '/components', name: 'components', component: ComponentsView },
   { path: '/cardeditor', name: 'cardeditor', component: CardEditorView },
+  { path: '/cards', name: 'cards', component: CardsView },
 ]
 
 const router = createRouter({
