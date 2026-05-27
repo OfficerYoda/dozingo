6dac4e646c22bd733083fb4ca748df00106a0f55
Colin Djerdak <c.djerdak@gmail.com>
2026-05-27 08:47:27 +0200

    Auth Pages

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,14 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
 import SettingsView from '@/views/SettingsView.vue'
 import ProfileView from '@/views/ProfileView.vue'
 import ComponentsView from '@/views/ComponentsView.vue'
 import CardEditorView from '@/views/CardEditorView.vue'
 import CardsView from '@/views/CardsView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
+  { path: '/login', name: 'login', component: LoginView },
+  { path: '/register', name: 'register', component: RegisterView },
   { path: '/settings', name: 'settings', component: SettingsView },
   { path: '/profile', name: 'profile', component: ProfileView },
   { path: '/components', name: 'components', component: ComponentsView },
---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,95 @@
+<template>
+    <section class="register">
+        <div class="container">
+            <div class="grid">
+                <div class="col-4 md-2 sm-0"></div>
+                <div class="card col-4 md-8 sm-12">
+                    <div class="heading mb-3">
+                        <h2 class="mb-0">Join the Game</h2>
+                        <small>Start your journey here at dozingo</small>
+                    </div>
+                    <form action="#">
+                        <div class="mb-3">
+                            <label for="username">Username</label>
+                            <div class="input-group">
+                                <span><User :size="20" /></span>
+                                <input type="text" id="username" required>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="email">Email</label>
+                            <div class="input-group">
+                                <span><Mail :size="20" /></span>
+                                <input type="email" id="email" required>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="password">Password</label>
+                            <div class="input-group">
+                                <span><KeyRound :size="20" /></span>
+                                <input type="password" id="password" required>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="confirm-password">Confirm Password</label>
+                            <div class="input-group">
+                                <span><KeyRound :size="20" /></span>
+                                <input type="password" id="confirm-password" required>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Log In</button>
+                    </form>
+                    <small class="login-notice">
+                        Already have an account? <RouterLink to="/login">Log In</RouterLink>
+                    </small>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style>
+    .register .card{
+        border-top: 5px solid var(--card-blue);
+    }
+
+    .heading{
+        text-align: center;
+    }
+
+    .heading small{
+        color: var(--color-text-subtle);
+    }
+
+    .register input{
+        background-color: var(--color-bg-card-tinted);
+        outline: none;
+        border: none;
+        border-radius: var(--radius-sm);
+        padding: 8px 10px;
+        display: block;
+        width: 100%;
+    }
+
+    .register button{
+        width: 100%;
+        border-radius: 999px !important;
+    }
+
+    .login-notice{
+        color: var(--color-text-subtle);
+        text-align: center;
+        display: block;
+        padding-top: 20px;
+        padding-bottom: 5px;
+    }
+
+    .login-notice a{
+        font-weight: 600;
+        color: var(--color-primary-600);
+    }
+</style>
+
+<script setup lang="ts">
+import { User, Mail, KeyRound } from 'lucide-vue-next'
+</script>
